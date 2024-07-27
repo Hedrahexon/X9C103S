f40b3529fc07cdc11b1955a35d2587c13e6657ab
Johan Thoma Jiji <johanthomasjiji@gmail.com>
2024-07-27 18:50:39 +0530

    Update increaseResistance.ino

--- examples/increaseResistance/increaseResistance.ino
+++ examples/increaseResistance/increaseResistance.ino
@@ -2,11 +2,13 @@
 
 #include <X9C103S.h>
 void setup() {
-  X9C103S pot1(6, 7, 8) //X9C103S digital potentiometer connected to the Arduino with inc pin to pin 6 ud pin to pin 7 and cs pin to pin 8. Change pin numbers as nessary.
-  pot1.initializePot()
+  Serial.begin(9600);
+  X9C103S pot1(6, 7, 8); //X9C103S digital potentiometer connected to the Arduino with inc pin to pin 6 ud pin to pin 7 and cs pin to pin 8. Change pin numbers as nessary.
+  pot1.initializePot();
 }
 
 void loop() {
-  pot1.setResistance(50) //Sets the pot resistance to 50. The number can be any number from 1 to 100 (both included).
-  pot1.increaseResistance(10) //Increases the pot resistance by 10. The resistance is now at the value 60.
+  pot1.setResistance(50); //Sets the pot resistance to 50. The number can be any number from 1 to 100 (both included).
+  pot1.increaseResistance(10); //Increases the pot resistance by 10. The resistance is now at the value 60.
+  Serial.write(pot1.getResistance()); //Gets the resistance of the pot and sends it via serial.
 }
